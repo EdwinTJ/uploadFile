8e1ab7e9161aa1fc85eddbd8c294b181dcb903cf
EdwinTJ <edwin16x@gmail.com>
2024-04-30 01:41:00 -0600

    Extrac file

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,39 @@
 import express from "express"
 import cors from "cors"
+import multer from "multer"
+import csvToJson from "convert-csv-to-json"
 
 const app = express()
 const port = process.env.PORT ?? 3000;
 
 app.use(cors()) // Enable cors
 
-app.post("/api/files", async( req,res)=>{
+// Set multer save in memory
+const storage = multer.memoryStorage()
+const upload = multer({ storage })
+
+let userData: Array<Record<string,string>> = []
+app.post("/api/files",upload.single("file"), async( req,res)=>{
     // TODO:
     // 1. Extract Values from request
+    const {file} = req
     // 2. Validate is not null or empty
+    if(!file){
+        return res.status(500).json({message:"File is Required"})
+    }
     // 3. Validete type file
+    if(file.mimetype != "text/csv"){
+        return res.status(500).json({message:"File must be CSV"})
+    }
     // 4. Convert file to a string
-    // 5. transform string to csv
+    try {
+        const result = Buffer.from(file.buffer).toString("utf-8");
+        // 5. transform string to csv
+        const csv = csvToJson.csvStringToJson(result)
+
+    } catch (error) {
+        
+    }
     // 6. Save JSON data
     // 7. Retrun 200 with a message and object wiht info.
 
