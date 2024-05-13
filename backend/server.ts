5ab00e049fda0798b4e219b0834dc00248e323e3
EdwinTJ <edwin16x@gmail.com>
2024-05-13 01:26:06 -0600

    Finish Upload

--- backend/server.ts
+++ backend/server.ts
@@ -1,76 +1,77 @@
-import express from "express"
-import cors from "cors"
-import multer from "multer"
-import csvToJson from "convert-csv-to-json"
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import csvToJson from "convert-csv-to-json";
 
-const app = express()
-const port = process.env.PORT ?? 3000
+const app = express();
+const port = process.env.PORT ?? 3000;
 
-app.use(cors()) // Enable cors
+app.use(cors()); // Enable cors
 
 // Set multer save in memory
-const storage = multer.memoryStorage()
-const upload = multer({ storage })
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-let userData: Array<Record<string,string>> = []
-app.post("/api/files",upload.single("file"), async( req,res)=>{
-    // TODO:
-    // 1. Extract Values from request
-    const {file} = req
-    // 2. Validate is not null or empty
-    if(!file){
-        return res.status(500).json({message:"File is Required"})
-    }
-    // 3. Validete type file
-    if(file.mimetype != "text/csv"){
-        return res.status(500).json({message:"File must be CSV"})
-    }
-    let json :Array<Record<string,string>> = []
-    try {
-        // 4. Convert file CSV to a string
-        const csv = Buffer.from(file.buffer).toString("utf-8")
-        // 5. transform string to json
-        json = csvToJson.csvStringToJson(csv)
-    } catch (error) {
-        return res.status(500).json({message:"Error parsing file"})
-    }
-    // 6. Save JSON data
-    userData = json
-    // 7. Retrun 200 with a message and object wiht info.
+let userData: Array<Record<string, string>> = [];
+app.post("/api/files", upload.single("file"), async (req, res) => {
+  // TODO:
+  // 1. Extract Values from request
+  const { file } = req;
+  // 2. Validate is not null or empty
+  if (!file) {
+    return res.status(500).json({ message: "File is Required" });
+  }
+  // 3. Validete type file
+  if (file.mimetype != "text/csv") {
+    return res.status(500).json({ message: "File must be CSV" });
+  }
+  let json: Array<Record<string, string>> = [];
+  try {
+    // 4. Convert file CSV to a string
+    const csv = Buffer.from(file.buffer).toString("utf-8");
+    console.log(csv);
+    // 5. transform string to json
+    json = csvToJson.fieldDelimiter(",").csvStringToJson(csv);
+  } catch (error) {
+    return res.status(500).json({ message: "Error parsing file" });
+  }
+  // 6. Save JSON data
+  userData = json;
+  // 7. Retrun 200 with a message and object wiht info.
+  console.log(userData);
+  return res
+    .status(200)
+    .json({ data: userData, message: "The file was successfully uploaded" });
+});
 
-    return res.status(200).json({data: userData,message:"The file was successfully uploaded"})
+app.get("/api/users", async (req, res) => {
+  //TODO:
+  // 1. Extract query param
+  const { q } = req.query;
+  // 2. Validate param
+  if (!q) {
+    return res.status(500).json({
+      message: 'Query parameter "q" is required',
+    });
+  }
+  if (Array.isArray(q)) {
+    return res.status(500).json({
+      message: 'Query parameter "q" is required',
+    });
+  }
+  // 3. Filter data with query
+  // The filter should search for partial matches and be case insensitive.
+  const search = q.toString().toLowerCase();
+  const filterData = userData.filter((row) => {
+    return Object.values(row).some((value) =>
+      value.toLowerCase().includes(search)
+    );
+  });
+  // 4. Retrun 200 with data filtered
 
-})
-
-app.get("/api/users",async (req,res)=>{
-    //TODO:
-    // 1. Extract query param
-    const {q} = req.query
-    // 2. Validate param
-    if(!q){
-        return res.status(500).json({
-            message :'Query parameter "q" is required'
-        })
-    }
-    if(Array.isArray(q)){
-        return res.status(500).json({
-            message :'Query parameter "q" is required'
-        })
-    }
-    // 3. Filter data with query
-    // The filter should search for partial matches and be case insensitive.
-    const search = q.toString().toLowerCase();
-    const filterData = userData.filter(row =>{
-        return Object
-        .values(row)
-        .some(value => value.toLowerCase().includes(search))
-    })
-    // 4. Retrun 200 with data filtered
-
-    return res.status(200).json({data : filterData})
-
-})
+  return res.status(200).json({ data: filterData });
+});
 // Start Server
-app.listen(port, () =>{
-    console.log(`Server Running at port:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Running at port:${port}`);
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,87 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import { uploadFile } from "./services/upload";
+import { Toaster, toast } from "sonner";
+import { type Data } from "./types";
+const APP_STATUS = {
+  IDLE: "idle",
+  ERROR: "error", // any error
+  READY_UPLOAD: "ready_upload", // choose file
+  UPLOADING: "uploading", // while upload
+  READY_USAGE: "ready_usage", // after upload
+} as const;
 
+const BUTTON_TXT = {
+  [APP_STATUS.READY_UPLOAD]: "Upload File",
+  [APP_STATUS.UPLOADING]: "Uploading File ...",
+};
+type AppStatusType = (typeof APP_STATUS)[keyof typeof APP_STATUS];
 function App() {
+  const [appStatus, setAppStatus] = useState<AppStatusType>(APP_STATUS.IDLE);
+  const [data, setData] = useState<Data>([]);
+  const [file, setFile] = useState<File | null>(null);
 
-  const handleInputChange =(event: React.ChangeEvent<HTMLInputElement>)=>{
-    const file = event.target.files?.[0]
-    console.log(file)
-  }
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      setFile(file);
+      setAppStatus(APP_STATUS.READY_UPLOAD);
+    }
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (appStatus !== APP_STATUS.READY_UPLOAD || !file) {
+      return;
+    }
+    setAppStatus(APP_STATUS.UPLOADING);
+
+    const [err, newData] = await uploadFile(file);
+    console.log(newData);
+    if (err) {
+      setAppStatus(APP_STATUS.ERROR);
+      toast.error(err.message);
+      return;
+    }
+    setAppStatus(APP_STATUS.READY_USAGE);
+    if (newData) {
+      setData(newData);
+      toast.success("File upload succesful");
+    }
+  };
+
+  const showButton =
+    appStatus === APP_STATUS.READY_UPLOAD || appStatus === APP_STATUS.UPLOADING;
+
+  const showInput = appStatus !== APP_STATUS.READY_USAGE;
   return (
     <>
-      <h4>Upload Challenge</h4>
+      <Toaster />
+      <h4>Upload Challenge + Search</h4>
       <div>
-      <form>
-        <label>
-        <input onChange={handleInputChange} name="file" type="file" accept='.csv'/>
-        </label>
+        {showInput && (
+          <form onSubmit={handleSubmit}>
+            <label>
+              <input
+                disabled={appStatus === APP_STATUS.UPLOADING}
+                onChange={handleInputChange}
+                name="file"
+                type="file"
+                accept=".csv"
+              />
+            </label>
 
-        <button>
-          Upload File
-        </button>
-      </form>
+            {showButton && (
+              <button disabled={appStatus === APP_STATUS.UPLOADING}>
+                {BUTTON_TXT[appStatus]}
+              </button>
+            )}
+          </form>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
